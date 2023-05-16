e5a63e4ecc3375aa9852cc23eccc17849948033d
Aleksandr Osyukov <ocukov@mail.ru>
2023-05-16 16:45:59 +0300

    change test selenide commit

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/jl1j8fsurb4bpsvv?svg=true)](https://ci.appveyor.com/project/osyualex/bankcardform)
+
+
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class bankcardformTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("chromedriver.chrome.driver", "./bankcardform/driver/chromedriver.exe");
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void FormAutomation() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id = 'name'] input")).sendKeys("Александр Соколов");
+        driver.findElement(By.cssSelector("[data-test-id = 'phone'] input")).sendKeys("+79589999999");
+        driver.findElement(By.cssSelector("[data-test-id = 'agreement'] input")).click();
+        driver.findElement(By.className("button__content")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("icon_theme")).getText().trim();
+        assertEquals(expected, actual);
+
+
+    }
+
+
+}
--- src/test/java/ru/netology/web/bankcardformTest.java
+++ src/test/java/ru/netology/web/bankcardformTest.java
@@ -1,54 +1,23 @@
 package ru.netology.web;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class bankcardformTest {
-
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("chromedriver.chrome.driver", "./bankcardform/driver/chromedriver.exe");
-
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
-
     @Test
-    void FormAutomation() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id = 'name'] input")).sendKeys("Александр Соколов");
-        driver.findElement(By.cssSelector("[data-test-id = 'phone'] input")).sendKeys("+79589999999");
-        driver.findElement(By.cssSelector("[data-test-id = 'agreement'] input")).click();
-        driver.findElement(By.className("button__content")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("icon_theme")).getText().trim();
-        assertEquals(expected, actual);
-
+    void FormAutomationTest() {
+        open("http://localhost:9999");
+        $("[data-test-id = 'name'] input").setValue("Александр Соколов");
+        $("[data-test-id = 'phone'] input").setValue("+79589999999");
+        $("[data-test-id = 'agreement']").click();
+        $(By.className("button__content")).click();
+        $("[data-test-id='order-success']").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
-
 
 }
